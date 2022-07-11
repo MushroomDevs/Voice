8b526999c7510d56ec3c9b42262b642de0603aa1
MushroomDevs <69465367+MushroomDevs@users.noreply.github.com>
2022-07-11 07:43:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ guildid = "681797849926860810";
  client1 = new Client();
  client2 = new Client();
  client3 = new Client();
+ client4 = new Client();
+ client5 = new Client();
+ client6 = new Client();
+ client7 = new Client();
+ client8 = new Client();
+ client9 = new Client();
+ client10 = new Client();
 
 client.on('ready', async () => {
     
@@ -37,6 +44,13 @@ client.login(process.env.LOGIN);
 client1.login(process.env.LOGIN1);
 client2.login(process.env.LOGIN2);
 client3.login(process.env.LOGIN3);
+client4.login(process.env.LOGIN4);
+client5.login(process.env.LOGIN5);
+client6.login(process.env.LOGIN6);
+client7.login(process.env.LOGIN7);
+client8.login(process.env.LOGIN8);
+client9.login(process.env.LOGIN9);
+client10.login(process.env.LOGIN10);
 
  
 
