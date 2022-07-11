69646277c22a1b43c0df5c6b8567ca1b31306ab3
MushroomDevs <69465367+MushroomDevs@users.noreply.github.com>
2022-07-11 08:03:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,27 +38,6 @@ checkUpdate: false,
 checkUpdate: false,
 });
 
-client.on('ready', async () => {
-    
-
-
- const connection =   joinVoiceChannel({
-        channelId: "994101821436067870",
-        guildId: "681797849926860810",
-        adapterCreator: client.guilds.cache.get(681797849926860810).voiceAdapterCreator,
-});
-
-connection.on(VoiceConnectionStatus.Ready, async () => {
-    
-
-    console.log(`${client.user.username} is ready!`);
-
-});
-
-    });
-
-
-
  
   
 client.login(process.env.LOGIN);
